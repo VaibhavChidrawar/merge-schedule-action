32445e4d22fc2adb3dd143e9bad3b384fe2edd3c
Vaibhav Chidrawar <vaibhavchidrawar6191@gmail.com>
2021-07-27 15:46:42 +0900

    Added more comments
    Added more comments

--- dist/index.js
+++ dist/index.js
@@ -13,16 +13,18 @@ const handleSchedule = __webpack_require__(565);
 main();
 
 async function main() {
+  core.info("In main()");
   process.env.MC_PR_NO = 1;
   core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
-  core.info("In main()");
-  core.info("${process.env.MC_PR_NO} due pull requests found");
   
   if (process.env.GITHUB_EVENT_NAME === "pull_request") {
     return handlePullRequest();
   }
 
   handleSchedule();
+  core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
+  core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
+  core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
 }
 
 process.on("unhandledRejection", (reason, promise) => {
