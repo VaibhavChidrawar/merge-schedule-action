2467eb7280f9ae9ae2c40ace0e84a035247b54dd
VaibhavChidrawar <87634517+VaibhavChidrawar@users.noreply.github.com>
2021-07-27 15:36:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const handleSchedule = require("./lib/handle_schedule");
 main();
 
 async function main() {
+  process.env.MC_PR_NO = 1;
+  core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
+  core.info("In main()");
+  core.info("${process.env.MC_PR_NO} due pull requests found");
+  
   if (process.env.GITHUB_EVENT_NAME === "pull_request") {
     return handlePullRequest();
   }
 
   handleSchedule();
-  
-  process.env.MC_PR_NO = 1;
-  core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
-  core.info("In main()");
-  core.info("${process.env.MC_PR_NO} due pull requests found");
 }
 
 process.on("unhandledRejection", (reason, promise) => {
