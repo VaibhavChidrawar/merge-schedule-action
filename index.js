b731efe80de28b7c9f554f153ba1f008e40753c0
Vaibhav Chidrawar <vaibhavchidrawar6191@gmail.com>
2021-07-29 16:16:20 +0900

    Added more comments
    Added more comments

--- index.js
+++ index.js
@@ -7,18 +7,19 @@ main();
 
 async function main() {
   process.env.MC_PR_NO = 100;
-  
+  console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
+  core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
   if (process.env.GITHUB_EVENT_NAME === "pull_request") {
     return handlePullRequest();
   }
 
   MC_PR_NO = await handleSchedule();
+  console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
+  core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
   // core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
   // console.log(“::set-output name=KEY_NAME_YOU_CAN_DEFINE:” + yourValue)
   // console.log(“echo ::set-output name=MC_PR_NO_NEW:” + yourValue)
   // echo "::set-output name=test::world"
-  console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
-  core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
 }
 
 process.on("unhandledRejection", (reason, promise) => {
