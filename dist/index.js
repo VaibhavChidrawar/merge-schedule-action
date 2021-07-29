3981fe613d31c86b0324319bac159e293c09a04d
Vaibhav Chidrawar <vaibhavchidrawar6191@gmail.com>
2021-07-29 16:23:23 +0900

    Added braces
    Added braces

--- dist/index.js
+++ dist/index.js
@@ -14,15 +14,15 @@ main();
 
 async function main() {
   MC_PR_NO = 100;
-  console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
-  core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
+  console.log("::set-output name=MC_PR_NO_NEW::${ MC_PR_NO }");
+  core.info("::set-output name=MC_PR_NO_NEWW::${{ MC_PR_NO }}");
   if (process.env.GITHUB_EVENT_NAME === "pull_request") {
     return handlePullRequest();
   }
 
   MC_PR_NO = await handleSchedule();
-  console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
-  core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
+  // console.log("::set-output name=MC_PR_NO_NEW::${MC_PR_NO}");
+  // core.info("::set-output name=MC_PR_NO_NEWW::${MC_PR_NO}");
   // core.info(`Merge conflict PR NO is ${process.env.MC_PR_NO}`);
   // console.log(“::set-output name=KEY_NAME_YOU_CAN_DEFINE:” + yourValue)
   // console.log(“echo ::set-output name=MC_PR_NO_NEW:” + yourValue)
